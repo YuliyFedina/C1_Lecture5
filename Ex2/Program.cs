16f5b8939247cac48c04f657bf93d0b4d59c5f9c
Федина Юлия <yu.belova@2gis.ru>
2019-06-27 08:37:47 +0700

    First Commit

---
+++ Ex2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
